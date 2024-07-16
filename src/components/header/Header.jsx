a051cbad6d1a94c646e8259baad5c721ecdb5a6a
RodrigoSobralM <rodrigo_sobral.m@hotmai.com>
2024-07-16 15:56:51 -0300

    :sparkles: feat: Estruturação e estilização finalizadas do componente header.

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div>
+        <header className='navbar'>
+            <nav>
+                <ul>
+                    <li>Organização</li>
+                    <li>Tarefas</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default Header
