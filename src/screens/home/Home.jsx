8babb9cdab7fca526cd1656d02753b9f61d51682
RodrigoSobralM <rodrigo_sobral.m@hotmai.com>
2024-07-16 20:03:03 -0300

    :sparkles: feat: Estruturação e estilização da page home finalizadas.

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,12 +1,64 @@
-import React from 'react'
-import Header from '../../components/header/Header'
+import React, { useState } from "react";
+import Header from "../../components/header/Header";
+import "./Home.css";
+import check from "../../assets/check.svg";
+import checked from "../../assets/checked.svg";
+import lapis from "../../assets/lapis.svg";
+import lixeira from "../../assets/lixeira.svg";
+import mais from "../../assets/mais.svg";
 
 const Home = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Limpar a casa", isChecked: false },
+    { id: 2, text: "Responder e-mails", isChecked: false },
+  ]);
+
+  const handleCheckedToggle = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, isChecked: !task.isChecked } : task
+      )
+    );
+  };
+
   return (
     <div>
-        <Header />
+      <Header />
+      <main className="contentPage">
+        <h1>Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
+        <div className="table">
+          <div className="tableHead">
+            <p>Tarefa</p>
+            <p>Status</p>
+            <p>Opções</p>
+          </div>
+          <div className="tableBody">
+            {tasks.map((task) => (
+              <div className="tableItem" key={task.id}>
+                <p>{task.text}</p>
+                <div className="checkbox">
+                  <img
+                    className=""
+                    src={task.isChecked ? checked : check}
+                    alt="Status"
+                    onClick={() => handleCheckedToggle(task.id)}
+                  />
+                </div>
+                <div className="displayFlexIcons">
+                  <img src={lapis} alt="Edit" />
+                  <img src={lixeira} alt="Delete" />
+                </div>
+              </div>
+            ))}
+            <div className="addItemTable">
+              <p>Nova tarefa...</p>
+              <img src={mais} alt="Add" />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
