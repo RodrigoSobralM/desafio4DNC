a211297ebb1eb27189142639bb951dd512c3a1f2
RodrigoSobralM <rodrigo_sobral.m@hotmai.com>
2024-07-16 20:46:44 -0300

    :sparkles: feat: Estruturação e estilização dos cards de exclusão e edição finalizadas.

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -13,6 +13,21 @@ const Home = () => {
     { id: 2, text: "Responder e-mails", isChecked: false },
   ]);
 
+  const [CardContents] = useState([
+    {
+      id: 1,
+      text: "Deseja editar esse item?",
+      description: "Colocar as descrições das tarefas aqui.",
+    },
+    {
+      id: 2,
+      text: "Deseja excluir esse item?",
+      description: "Colocar as descrições das tarefas aqui.",
+    },
+  ]);
+
+  const [activeCard, setActiveCard] = useState(null);
+
   const handleCheckedToggle = (id) => {
     setTasks((prevTasks) =>
       prevTasks.map((task) =>
@@ -21,40 +36,63 @@ const Home = () => {
     );
   };
 
+  const handleCardOpen = (id) => {
+    setActiveCard(id);
+  };
+
+  const handleCardClose = () => {
+    setActiveCard(null);
+  };
+
   return (
     <div>
       <Header />
       <main className="contentPage">
         <h1>Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
         <div className="table">
-          <div className="tableHead">
-            <p>Tarefa</p>
-            <p>Status</p>
-            <p>Opções</p>
-          </div>
-          <div className="tableBody">
-            {tasks.map((task) => (
-              <div className="tableItem" key={task.id}>
-                <p>{task.text}</p>
-                <div className="checkbox">
-                  <img
-                    className=""
-                    src={task.isChecked ? checked : check}
-                    alt="Status"
-                    onClick={() => handleCheckedToggle(task.id)}
-                  />
-                </div>
-                <div className="displayFlexIcons">
-                  <img src={lapis} alt="Edit" />
-                  <img src={lixeira} alt="Delete" />
+          {activeCard === null ? (
+            <>
+              <div className="tableHead">
+                <p>Tarefa</p>
+                <p>Status</p>
+                <p>Opções</p>
+              </div>
+              <div className="tableBody">
+                {tasks.map((task) => (
+                  <div className="tableItem" key={task.id}>
+                    <p>{task.text}</p>
+                    <div className="checkbox">
+                      <img
+                        className=""
+                        src={task.isChecked ? checked : check}
+                        alt="Status"
+                        onClick={() => handleCheckedToggle(task.id)}
+                      />
+                    </div>
+                    <div className="displayFlexIcons">
+                      <img src={lapis} alt="Edit" onClick={() => handleCardOpen(1)} />
+                      <img src={lixeira} alt="Delete" onClick={() => handleCardOpen(2)} />
+                    </div>
+                  </div>
+                ))}
+                <div className="addItemTable">
+                  <p>Nova tarefa...</p>
+                  <img src={mais} alt="Add" />
                 </div>
               </div>
-            ))}
-            <div className="addItemTable">
-              <p>Nova tarefa...</p>
-              <img src={mais} alt="Add" />
-            </div>
-          </div>
+            </>
+          ) : (
+            CardContents.filter((card) => card.id === activeCard).map((card) => (
+              <div className="cardEditarTask" key={card.id}>
+                <p>{card.text}</p>
+                <p>{card.description}</p>
+                <div className="displayFlexButtons">
+                  <button onClick={handleCardClose}>Não</button>
+                  <button onClick={handleCardClose}>Sim</button>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </main>
     </div>
